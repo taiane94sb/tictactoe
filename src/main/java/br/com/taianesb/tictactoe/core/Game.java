f236e308977a8f44bbc118c7f0cd7744997ff582
taiane94sb <taiane19sb94@gmail.com>
2021-08-30 21:31:25 -0300

    creating game players

--- src/main/java/br/com/taianesb/tictactoe/core/Game.java
+++ src/main/java/br/com/taianesb/tictactoe/core/Game.java
@@ -11,8 +11,19 @@ public class Game {
     public void play() {
         UI.printGameTitle();
 
-        board.print();
+        for (int i = 0; i < players.length; i++) {
+            players[i] = createPlayer(i);
+        }
 
-//        UI.readInput("Nome do Jogador: ");
+    }
+
+    public Player createPlayer(int index) {
+        String name = UI.readInput("Jogador " + (index + 1) + " = > ");
+        char symbol = Constants.SYMBOL_PLAYERS[index];
+        Player player = new Player(name, board, symbol);
+
+        UI.printText("O jogador + '" + name + "' vai usar o símbolo '" + symbol + "'");
+
+        return player;
     }
 }
